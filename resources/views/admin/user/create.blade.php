d8c34d575366d8e9fd2b4dde8fa2bc8873487d7b
RyzGa <2310501017@mahasiswa.upnvj.ac.id>
2025-11-13 15:02:43 +0700

    blog add blog create view and controller

---
+++ resources/views/admin/user/create.blade.php
@@ -0,0 +1,30 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="card">
+        <card-header>
+            <h5>{{ $title ?? '' }}</h5>
+        </card-header>
+    </div>
+    <div class="card-body">
+        <form action="{{route('user.store')}}"
+        method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Full Name</label>
+                <input type="text" name="name" class="form-control" placeholder="Enter your full name" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" placeholder="Enter your email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Enter your password" required>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary">Save</button>
+            </div>
+               
+        </form>
+    </div>
+@endsection
---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="card">
+        <card-header>
+            <h5>{{ $title ?? '' }}</h5>
+        </card-header>
+    </div>
+    <div class="card-body">
+        <form action="{{route('user.update', $edit->id)}}"
+        method="post">
+            @csrf
+            @method('put')
+            <div class="mb-3">
+                <label for="name" class="form-label">Full Name</label>
+                <input type="text" name="name" class="form-control" placeholder="Enter your full name" value="{{ $edit->name }}">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" placeholder="Enter your email" value="{{ $edit->email }}">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Enter your password">
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary">Save</button>
+            </div>
+               
+        </form>
+    </div>
+@endsection
